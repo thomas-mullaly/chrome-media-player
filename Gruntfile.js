0a58315a3926ccbfe28ee905aacb0ca7b68865ca
Thomas Mullaly <tmullaly88@gmail.com>
2014-09-16 22:56:31 -0500

    Setup stub build file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    src: ['**/*.js', 'manifest.json', '**/*.html'],
+                    dest: 'build',
+                    cwd: "src/"
+                }]
+            }
+        }
+    });
 
-    })
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.registerTask("default", ["dev"]);
+    grunt.registerTask("dev", ["copy:dev"]);
 };
